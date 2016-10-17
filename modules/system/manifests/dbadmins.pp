37342f2f83fd74dcba051980dca786a2db9b857d
jam <jam@puppetlabs.vm>
2016-10-17 21:21:22 +0000

    db refactor

--- modules/system/manifests/dbadmins.pp
+++ modules/system/manifests/dbadmins.pp
@@ -1,22 +1,19 @@
 class system::dbadmins {
 require mysql::server
-mysql_user { 'zack@localhost':
-ensure => present,
-max_queries_per_hour => 1200,
-}
+
 mysql_user { 'monica@localhost':
-ensure => present,
+ensure              => present,
 max_queries_per_hour => 600,
-}
+} ->
+mysql_user { 'brad@localhost':
+} ->
+mysql_user { 'luke@localhost':
+} ->
+mysql_user { 'zack@localhost':
+max_queries_per_hour => 1200,
+} ->
 mysql_user { 'ralph@localhost':
 ensure => absent,
 }
-mysql_user { 'brad@localhost':
-ensure => present,
-max_queries_per_hour => 600,
-}
-mysql_user { 'luke@localhost':
-ensure => present,
-max_queries_per_hour => 600,
-}
+
 }
