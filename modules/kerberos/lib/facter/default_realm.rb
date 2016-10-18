490255b8a8a98e634a8a3ca950fd97a1d5f8e0a8
jam <jam@puppetlabs.vm>
2016-10-18 21:30:02 +0000

    facter aug

--- modules/kerberos/lib/facter/default_realm.rb
+++ modules/kerberos/lib/facter/default_realm.rb
@@ -1,3 +1,8 @@
-Facter.add("default_realm") do
-  setcode "/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf"
+require 'augeas'
+Facter.add('default_realm') do
+setcode do
+Augeas::open do |aug|
+aug.get('/files/etc/krb5.conf/libdefaults/default_realm')
+end
+end
 end
