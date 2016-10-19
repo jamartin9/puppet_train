21297dc3fb6535663dd15342fae39639d2b80772
jam <jam@puppetlabs.vm>
2016-10-19 17:29:23 +0000

    dupe

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -1,7 +1,3 @@
 class profile::base {
-#  notify { "Hello ${fqdn}": }
-  $message = hiera('message')
-  notify { $message: }
   notify { "IN BASE ROLE": }
-#  include hosts
 }
--- modules/profile/manifests/jam.pp
+++ modules/profile/manifests/jam.pp
@@ -1,7 +1,3 @@
 class profile::jam inherits profile::base {
-#  notify { "Hello ${fqdn}": }
-  $message = hiera('message')
-  notify { $message: }
   notify { "IN JAM PROFILE": }
-#  include hosts
 }
